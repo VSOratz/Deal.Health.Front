118a6fa969cab5e7a01d638eb5ddd21810510955
Dougllas Souza <douglas.souza@deal.com.br>
2022-12-09 08:12:54 -0300

    #inicio_da_tela_concluida

--- src/app/views/new-patient/new-patient.module.ts
+++ src/app/views/new-patient/new-patient.module.ts
@@ -3,13 +3,26 @@ import { CommonModule } from '@angular/common';
 
 import { NewPatientRoutingModule } from './new-patient-routing.module';
 import { NewPatientComponent } from './new-patient.component';
+import { LayoutModule } from 'src/app/shared/layout/layout.module';
+import { MatIconModule } from '@angular/material/icon';
+import {MatTabsModule} from '@angular/material/tabs';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 
 @NgModule({
   declarations: [NewPatientComponent],
   imports: [
     CommonModule,
-    NewPatientRoutingModule
+    NewPatientRoutingModule,
+    LayoutModule,
+    MatIconModule,
+    MatTabsModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatFormFieldModule
   ]
 })
 export class NewPatientModule { }
